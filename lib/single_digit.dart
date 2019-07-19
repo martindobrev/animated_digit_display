e42beaac2314178adff0e786ee2299cfe2d7a529
martin.dobrev@noser.com <LEviZU87>
2019-07-19 10:22:03 +0300

    step0-3 basic layout displaying all digits
    Text widgets for each digit added with custom styling

--- lib/single_digit.dart
+++ lib/single_digit.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
 class SingleDigit extends StatefulWidget {
+
+  final TextStyle textStyle;
+  final BoxDecoration boxDecoration;
+
+  SingleDigit({ 
+    this.boxDecoration: const BoxDecoration(color: Colors.black), 
+    this.textStyle: const TextStyle(color: Colors.grey, fontSize: 30)
+  });
+  
   @override
-  _SingleDigitState createState() => _SingleDigitState();
+  State<StatefulWidget> createState() {
+    return new _SingleDigitState(textStyle, boxDecoration);
+  }
 }
 
 class _SingleDigitState extends State<SingleDigit> {
+
+  _SingleDigitState(this._textStyle, this._boxDecoration);
+
+  final TextStyle _textStyle;
+  final BoxDecoration _boxDecoration;
+  
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        for (var i = 0; i < 10; i++)
-          Text(i.toString())
-      ],
+    return Container(
+      decoration: _boxDecoration,
+      child: Column(
+        children: <Widget>[
+          for (var i = 0; i < 10; i++)
+            Text(i.toString(), style: _textStyle)   // ADD STYLE TO THE TEXT 
+        ],
+      ),
     );
   }
 }
